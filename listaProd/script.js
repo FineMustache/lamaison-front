b6981a6d42c42344ed00af53e7c1cb23d87a67c9
Guilherme Mendes <guihenrique2806@gmail.com>
2023-04-17 19:04:53 -0300

    Pagina de Produtos + Alteração no Header

--- listaProd/script.js
+++ listaProd/script.js
@@ -1,4 +1,17 @@
 const urlTag = new URLSearchParams(window.location.search).get('tag')
+const curPage = new URLSearchParams(window.location.search).get('page') ? new URLSearchParams(window.location.search).get('page') : 1
+
+if (urlTag !== null) {
+  document.querySelector('#mtTag').innerHTML = urlTag 
+} else {
+  document.querySelector('.main-title').classList.add('escondido')
+}
+
+document.querySelector('.paginacao').querySelectorAll('a').forEach(a => {
+  if (a.innerHTML == curPage) {
+    a.parentElement.classList.add('cur-page')
+  }
+})
 
 function hoverFav(index, el) {
     if (!el.classList.contains('cur-item-fav-on')) {
@@ -23,6 +36,21 @@ function hoverFav(index, el) {
     document.querySelector('.cart-container').classList.toggle('escondido')
   }
 
-  function carregar() {
-    document.querySelector('#mtTag').innerHTML = urlTag
+  function setFav(el){
+    el.classList.toggle('fa-regular')
+    el.classList.toggle('fa-solid')
+    el.classList.toggle('item-fav-on')
+  }  
+  
+  function hoverFav(index, el) {
+    if (!el.classList.contains('cur-item-fav-on')) {
+        if (index == 1) {
+            el.classList.remove('fa-regular')
+            el.classList.add('fa-solid')
+        } else {
+            el.classList.add('fa-regular')
+            el.classList.remove('fa-solid')
+        }
+    }
+    
   }
